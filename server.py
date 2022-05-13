eed2e3a8067fae390a19dd402d05cf87c9669009
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-05-13 18:50:15 +0300

    Filling /static direction with css files

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return '<h1 style="text-align: center" >Flask Web Page</h1>'
-
+def home():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
